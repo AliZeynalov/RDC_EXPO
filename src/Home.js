77214388a1af8988474bf6408e2a6ecfd5c8af7b
Ali Zeynalov <ali.yasharoglu@gmail.com>
2017-09-10 17:17:20 +0300

    native-base imported and configured, main page styles updated.

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React, {Component,PropTypes} from 'react';
 import {NavigatorIOS,ListView, TextInput, View, Image, Platform, TouchableOpacity, ScrollView, StyleSheet,Animated,Alert,ActivityIndicator,AsyncStorage,Dimensions,Text} from 'react-native';
 import MapPage from './MapPage';
+import {Container, Form, H3, Footer, Header, Icon, Content, Body, Left, Right, Button, Input, Item, Spinner, CardItem, Thumbnail} from 'native-base';
 const {width, height} = Dimensions.get("window"),
     vw = width / 100
 vh = height / 100
@@ -40,14 +41,16 @@ export default class Home extends Component {
             return null;
 
         return(
-            // <ProductItem name={post.name} price={post.price} thumbnail={post.cover_url} addItem={()=>this.addItem(post)}/>
-            <View>
-                <Text>{post.name}</Text>
-                <TouchableOpacity onPress={()=>this.navigateTo(post)}>
-                <Image style={{width: 80, height: 80}} source={{uri: post.img}}/>
-                <Text>{post.description}</Text>
-                </TouchableOpacity>
-            </View>
+
+        <CardItem horizontal={false}
+            style={{flexDirection: 'column',borderWidth: 1,borderRadius: 4,padding: 0,marginRight: 14,
+            borderColor: '#E5DDCB',height: 180, margin: 6,width: (width-36)/3}}>
+            <TouchableOpacity onPress={()=>this.navigateTo(post)}>
+            <Thumbnail square size={40} source={{uri: post.img }} style={{marginTop: 6, marginBottom: 6}} />
+            <Text style={{color: '#E5DDCB',fontSize: 20, fontWeight: 'bold'}}>{post.name}</Text>
+            <Text style={{marginBottom: 8}}>{post.description}</Text>
+            </TouchableOpacity>
+        </CardItem>
         );
 
 
@@ -80,23 +83,28 @@ export default class Home extends Component {
     {
 
         return(
-            <View style={styles.container}>
-                <View style={styles.topBar}>
+            <Container style={styles.container}>
+                <Header style={{"backgroundColor": '#524656',elevation: 0}}>
+                    <Body style={{flex: 4}}>
                     <Text style={{letterSpacing: 0.5,color: '#E5DDCB'}}>RDC Partner</Text>
-                </View>
+                    </Body>
+                </Header>
+                {/*<View style={styles.topBar}>*/}
+                    {/*<Text style={{letterSpacing: 0.5,color: '#E5DDCB'}}>RDC Partner</Text>*/}
+                {/*</View>*/}
             <View style={{flex: 1,
                 flexDirection: 'column',
                 justifyContent: 'space-between'}}>
-                <ScrollView style={{backgroundColor: '#E8E8E8', flex: 1}}>
+                <Content contentContainerStyle={{flexDirection: 'row',flex: 1, flexWrap: 'wrap', left: 5}}>
                     <ListView contentContainerStyle={styles.grid}
                       dataSource={this.state.dataSource}
                       enableEmptySections={true}
                       horizontal
                       renderRow={(rowData)=>this.renderRow(rowData)}>
                     </ListView>
-                </ScrollView>
+                </Content>
             </View>
-            </View>
+            </Container>
         );
     }
 
